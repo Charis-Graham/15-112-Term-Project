048c9115fba231e6499964dddc287298b51ff3a4
csgraham <mscharissgraham@gmail.com>
2022-11-18 19:06:55 -0500

    Created classes, added elephant png

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Elephant(object):
+    pass
+
+class WateringHole(object):
+    pass
+
+class Tree(object):
+    pass
+
+class Grass(object):
+    pass
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #6:40
 
+from cmu_112_graphics import *
+
+def appStarted(app):
+    pass
+
+runApp(width = 400, height = 400)
