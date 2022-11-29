1c9ab43440d5a5104a92fe45f1ff28ac5e6e38ab
csgraham <116227603+Charis-Graham@users.noreply.github.com>
2022-11-29 02:41:24 -0500

    added to game modes

--- classes.py
+++ classes.py
@@ -2,6 +2,10 @@ from cmu_112_graphics import *
 from elephantPlayer import *
 import random
 
+class hitBoxInheritance(object):
+    pass
+#use 15112 Lecture 30 hitbox as example
+
 class Elephant(object):
     def __init__(self, app, lifeState, hunger, thirst, energy):
         #life stats of the elephant
@@ -214,21 +218,3 @@ class Tree(object):
                 r,g,b = oldImage.getpixel((x,y))
                 recolorImage.putpixel((x,y), (r, g, 0))
         self.image = recolorImage
-
-#class to be used as a hitbox to determine when elements are interacting
-class hitBox(object):
-    def __init__(self, imgWidth, imgHeight, imgX, imgY):
-        self.imgWidth = imgWidth
-        self.imgHeight = imgHeight
-        self.imgX = imgX
-        self.imgY = imgY
-
-    #defines the parameters of a hitbox for a given sprite
-    def makeHitBox(self):
-        #returns a tuple with a rectangular box value for the hitbox
-        return (self.imgX, self.imgY, self.imgX + self.imgWidth, 
-                    self.imgY+self.imgHeight)
-    
-    #will determine if two hitboxes are overlapping
-    def overlap(self, other):
-        pass
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def appStarted(app):
     helpScreenMode_initiate(app)
     gameMode_initiate(app)
 
+    #game mode based on idea from Using Modes (aka Screens) Example on
+    #https://www.cs.cmu.edu/~112/notes/notes-animations-part4.html#usingModes 
     app.mode = "startScreenMode"
 
 runApp(width = 400, height = 400)
