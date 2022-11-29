92a042a449ce475d6035081dadd41229b2829885
csgraham <116227603+Charis-Graham@users.noreply.github.com>
2022-11-29 01:32:35 -0500

    s

---
+++ startscreen.py
@@ -0,0 +1,9 @@
+from cmu_112_graphics import *
+
+def startScreen_redrawAll(app, canvas):
+    pass
+#image taken from
+#https://www.livescience.com/27320-elephants.html
+
+def startButton(app, canvas):
+    pass
