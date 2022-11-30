4b66d5a101b20e29a109fb2b867d48791c08f28a
csgraham <116227603+Charis-Graham@users.noreply.github.com>
2022-11-29 20:07:22 -0500

    d

--- gamemode.py
+++ gamemode.py
@@ -7,7 +7,7 @@ from helpscreen import *
 
 def gameMode_initiate(app):
     #draw grass
-    grass(app)
+    gameMode_grass(app)
 
     #draws a tree
     app.tree = Tree(100)
@@ -23,24 +23,6 @@ def gameMode_initiate(app):
     app.player.elephantWalking(app)
     app.player.elephantStandStill(app)
 
-#Copied from game object class developed in Lecture 1
-#Tuesday, November 22, 2022 by Pat Virtue
-#which was based on side scroller #3 on 
-#https://www.cs.cmu.edu/~112/notes/notes-animations-part4.html#sidescrollerExamples
-def gameMode_getBounds(self):
-    # returns absolute bounds, not taking scrollX into account
-    (x0, y1) = (self.x, self.height/2 - self.y)
-    (x1, y0) = (x0 + self.width, y1 - self.height)
-    return x0, y0, x1, y1
-    
-def gameMode_intersectsObject(self, other):
-    # return l2<=r1 and t2<=b1 and l1<=r2 and t1<=b2
-    (ax0, ay0, ax1, ay1) = self.getBounds
-    (bx0, by0, bx1, by1) = other.getBounds
-    return ((ax1 >= bx0) and (bx1 >= ax0) and
-            (ay1 >= by0) and (by1 >= ay0))
-
-#------
 
 def gameMode_keyPressed(app, event):
     #controls the player moving
@@ -58,10 +40,28 @@ def gameMode_keyPressed(app, event):
         app.tree.goLeaves(app)
     elif event.key == "h":
         app.mode = "helpScreenMode"
-    
+
 def gameMode_keyReleased(app, event):
     #controls what happens when the elephant is still
     playerStill(app, event)
+
+#Copied from game object class developed in Lecture 1
+#Tuesday, November 22, 2022 by Pat Virtue
+#which was based on side scroller #3 on 
+#https://www.cs.cmu.edu/~112/notes/notes-animations-part4.html#sidescrollerExamples
+def gameMode_getBounds(self):
+    # returns absolute bounds, not taking scrollX into account
+    (x0, y1) = (self.x, self.height/2 - self.y)
+    (x1, y0) = (x0 + self.width, y1 - self.height)
+    return x0, y0, x1, y1
+    
+def gameMode_intersectsObject(self, other):
+    # return l2<=r1 and t2<=b1 and l1<=r2 and t1<=b2
+    (ax0, ay0, ax1, ay1) = self.getBounds
+    (bx0, by0, bx1, by1) = other.getBounds
+    return ((ax1 >= bx0) and (bx1 >= ax0) and
+            (ay1 >= by0) and (by1 >= ay0))
+#------
 
 def gameMode_timerFired(app):
     #this runs through the sprite to have the elephant walk at a constant pace
@@ -77,7 +77,7 @@ def gameMode_timerFired(app):
 
 def gameMode_redrawAll(app, canvas):
     #draws the ground
-    drawGround(app, canvas)
+    gameMode_drawGround(app, canvas)
     #draws a tree
     app.tree.drawTree(app, canvas)
     #draw water
@@ -91,4 +91,3 @@ def gameMode_redrawAll(app, canvas):
     else:
         app.player.drawElephantWalk(app, canvas)
         app.player.elephantWeaken()
-
--- graphics.py
+++ graphics.py
@@ -1,7 +1,7 @@
 from cmu_112_graphics import *
 
 #GRASS BACKGROUND
-def grass(app):
+def gameMode_grass(app):
     #draws the ground
     #Grass PNGs come from https://opengameart.org/content/grass-texture-pack
     #Under CC0
@@ -13,8 +13,11 @@ def grass(app):
     app.grassImageWidth = sizeIm[0]
 
 #draws the background of the game
-def drawGround(app, canvas):
+def gameMode_drawGround(app, canvas):
+    imageGrass = ImageTk.PhotoImage(app.grassImage)
     for dw in range(0, app.width, 384):
-        for dh in range(0, app.height, 380):
+        for dh in range(0, app.height, 384):
             canvas.create_image(dw, dh,
-                                image = ImageTk.PhotoImage(app.grassImage))
+                                image = imageGrass)
+
+#spawn trees
